ef8a4e38e91f8b7b9e65c89e28df2f5c8fc49f5f
Vidhyacoder123 <vidyamvidyashreem@gmail.com>
2025-11-18 12:09:48 +0530

    Create do while.c

---
+++ do while.c
@@ -0,0 +1,21 @@
+int main()
+{
+    int n,i=2, flag=0;
+    printf("enter an int number\n");
+    scanf("%d",&n);
+    do
+}
+if(n%i==0)
+{
+    flag=1;
+    break;
+}
+i++;
+}
+while (i<n/2);
+if(flag==o)
+    printf("it is an prime\n");
+  else
+  printf("not a prime\n");
+    return 0;
+}
