3af38f774634b8f29c0187ef940ec25d2cba4448
Vidhyacoder123 <vidyamvidyashreem@gmail.com>
2025-11-18 16:09:58 +0530

    Create userdetails.c

---
+++ userdetails.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int userdetails(){
+    printf("my name is:vidhyashree.m");
+    printf("my age is:18");
+    printf("my department:CSE");
+}
+int main() {
+    userdetails();
+}
