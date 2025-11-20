4996c53bc4322d75d8066eef5a78cb50a5a284c8
Vidhyacoder123 <vidyamvidyashreem@gmail.com>
2025-11-20 11:05:24 +0530

    Create vidhya.c

---
+++ vidhya.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char vidhya[10];
+    printf("enter your string\n");
+    scanf("%d",&vidhya);
+    printf("%s\n",vidhya);
+    strcat(vidhya,"queen");
+    printf("%s",vidhya);
+
+    return 0;
+}
