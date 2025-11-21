9da1c087d9838a53d4dab679467471aa680c71b4
Vidhyacoder123 <vidyamvidyashreem@gmail.com>
2025-11-21 09:15:53 +0530

    Create Increment.c

---
+++ Increment.c
@@ -0,0 +1,12 @@
+Program:9: incriment pointer 
+#include<stdio.h>
+int main(){
+    int x[] ={10,20,30};
+    int *p = x;
+    printf("%d\n",*p);
+    p++;
+   printf("%d\n",*p);
+    p++; 
+printf("%d\n",*p);
+    p++; 
+}
