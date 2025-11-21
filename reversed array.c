d45dd56bd8e492f8e792d512320f63566f70a1a1
Vidhyacoder123 <vidyamvidyashreem@gmail.com>
2025-11-21 12:50:48 +0530

    Create reversed array.c

---
+++ reversed array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main() {
+  int arr[100],n=5;
+  printf("%d",n);
+  for(int i=0;i<5;i++){
+   scanf("%d",&arr[i]);
+}
+ for(int i=0;i<5;i--)
+ printf("%d",arr[i]);
+ }
